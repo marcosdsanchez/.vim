a27f73e1773a3cc3b041d1e3ab56dd4025b0b29f
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2012-11-11 01:12:21 -0300

    Install vundle in bootstrap and create backup dirs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
+mkdir -p $HOME/.vim/tmp/{backup,swap}
+mkdir -p $HOME/.vim/undo
