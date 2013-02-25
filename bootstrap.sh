aa890ff9273f54a601221e79da32bfb89356fe67
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-02-25 16:31:27 -0300

    * Adds patched font to installer. Auto install plugins. * Set textwidth to 120 * Decrease text font by one

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone git://gist.github.com/1630581.git ~/.fonts/ttf-dejavu-powerline
 
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 mkdir -p $HOME/.vim/tmp/{backup,swap}
 mkdir -p $HOME/.vim/undo
+vim +BundleInstall +qa
